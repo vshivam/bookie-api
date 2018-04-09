2b27747c12e6a2da9bc02da0e7e5f3c60833e6a7
Shivam Verma <shivam.bitsgoa@gmail.com>
2018-04-09 10:28:37 +0200

    init

---
+++ instance/config.py
@@ -0,0 +1,21 @@
+# /instance/config.py
+
+import os
+
+class Config(object):
+  DEBUG = False
+  CSRF_ENABLED = True
+  SECRET = "BOOKIE-AMAZING-SECRET"
+  SQLALCHEMY_DATABASE_URI = "postgresql://localhost/flask_api"
+
+
+class DevelopmentConfig(Config):
+  DEBUG = True
+
+class ProductionConfig(Config):
+  DEBUG = False
+
+app_config = {
+  "development": DevelopmentConfig, 
+  "production": ProductionConfig
+}
